50938d536dfbdfe1728a6dbc922dcdbb88157cb9
goit-latam <126070957+goit-latam@users.noreply.github.com>
2023-07-04 08:56:23 +0200

    done

---
+++ src/components/App.js
@@ -0,0 +1,42 @@
+import { Component } from 'react';
+import { Layout } from './Layout';
+import { BreedSelect } from './BreedSelect';
+import { fetchDogByBreed } from 'api';
+import { Dog } from './Dog';
+import { ErrorMessage } from './ErorrMessage';
+import { DogSkeleton } from './DogSkeleton';
+
+const ERROR_MSG =
+  'No pudimos recopilar datos sobre el perro, intente nuevamente 😇';
+
+export class App extends Component {
+  state = {
+    dog: null,
+    isLoading: false,
+    error: null,
+  };
+
+  fetchDog = async breedId => {
+    try {
+      this.setState({ isLoading: true, error: null });
+      const fetchedDog = await fetchDogByBreed(breedId);
+      this.setState({ dog: fetchedDog });
+    } catch (error) {
+      this.setState({ error: ERROR_MSG });
+    } finally {
+      this.setState({ isLoading: false });
+    }
+  };
+
+  render() {
+    const { dog, isLoading, error } = this.state;
+    return (
+      <Layout>
+        <BreedSelect onSelect={this.fetchDog} />
+        {isLoading && <DogSkeleton />}
+        {error && <ErrorMessage>{error}</ErrorMessage>}
+        {dog && !isLoading && <Dog dog={dog} />}
+      </Layout>
+    );
+  }
+}
---
+++ src/components/BreedSelect.js
@@ -0,0 +1,46 @@
+import { Component } from 'react';
+import Select from 'react-select';
+import { fetchBreeds } from 'api';
+import { ErrorMessage } from './ErorrMessage';
+
+const ERROR_MSG =
+  'Algo salió mal, recarga la página  🥹';
+
+export class BreedSelect extends Component {
+  state = {
+    breeds: [],
+    isLoading: false,
+    error: null,
+  };
+
+  async componentDidMount() {
+    try {
+      this.setState({ isLoading: true, error: null });
+      const fetchedBreeds = await fetchBreeds();
+      this.setState({ breeds: fetchedBreeds });
+    } catch (error) {
+      this.setState({ error: ERROR_MSG });
+    } finally {
+      this.setState({ isLoading: false });
+    }
+  }
+
+  render() {
+    const { breeds, isLoading, error } = this.state;
+    const options = breeds.map(breed => ({
+      value: breed.id,
+      label: breed.name,
+    }));
+
+    return (
+      <div>
+        <Select
+          options={options}
+          isLoading={isLoading}
+          onChange={option => this.props.onSelect(option.value)}
+        />
+        {error && <ErrorMessage>{error}</ErrorMessage>}
+      </div>
+    );
+  }
+}
